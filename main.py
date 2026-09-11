a2cb8e5f4ddaee37881f5651a5ead70978563c1e
ian <x7814778r@yahoo.com.tw>
2026-09-12 05:03:12 +0800

    v1.0.9

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ import multiprocessing
 # ==========================================
 # 設定區
 # ==========================================
-APP_VERSION = "v1.0.8.1"
+APP_VERSION = "v1.0.9"
 
 if getattr(sys, 'frozen', False):
     BASE_DIR = os.path.dirname(sys.executable) 
