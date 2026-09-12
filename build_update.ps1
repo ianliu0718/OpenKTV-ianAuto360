f067cee5e7d0e04a60d94f444f6c939a3920f7e5
ian <x7814778r@yahoo.com.tw>
2026-09-12 20:59:54 +0800

    v1.0.9.8

--- build_update.ps1
+++ build_update.ps1
@@ -6,7 +6,7 @@ $ErrorActionPreference = "Stop"
 
 $ProjectDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 Set-Location $ProjectDir
-$ReleaseVersion = "v1.0.9.7"
+$ReleaseVersion = "v1.0.9.8"
 $AppName = "ianAutoKTV_Server"
 $DistRoot = Join-Path $ProjectDir "dist"
 $BuildDir = Join-Path $DistRoot "ianAutoKTV_Server"
--- main.py
+++ main.py
@@ -61,7 +61,7 @@ import multiprocessing
 # ==========================================
 # 設定區
 # ==========================================
-APP_VERSION = "v1.0.9.7"
+APP_VERSION = "v1.0.9.8"
 
 if getattr(sys, 'frozen', False):
     BASE_DIR = os.path.dirname(sys.executable) 
